b02690ccd71c837b2a63b4f1a2cb585d817e88ca
Thatix <125364717+Thatix@users.noreply.github.com>
2023-02-28 08:30:03 -0300

    aula sobre estrutura de decisão e repetição

---
+++ aula5/repeticao_for.php
@@ -0,0 +1,17 @@
+<h2> Estrutura de repetição - for</h2>
+<p>
+    Utilizamos para de repetir um conjunto de instruções por um 
+    número determinado de vezes.
+</p>
+<storng>Exemplo</strong>
+<pre>
+    for(inicio, condição, passo){
+        //comandos a serem repetidos.
+    }
+</pre>
+
+<?php 
+    for($contador = 1; $contador < 10; $contador++){
+        echo "5 x $contador = <br>";
+    }
+?>
