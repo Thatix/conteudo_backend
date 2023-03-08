a1c4e477b755e8d465ea1b0dc24e6c19d4892ee6
Thatix <thais1.carvalho@alunos.ifsuldeminas.edu.br>
2023-03-08 10:30:09 -0300

    Repetição for

--- aula5/repeticao_for.php
+++ aula5/repeticao_for.php
@@ -1,17 +1,35 @@
-<h2> Estrutura de repetição - for</h2>
+<style>
+  .grid{
+     display: grid;
+     grid-template-rows: auto auto;
+     grid-template-columns: auto auto auto auto auto;
+     gap: 10px;
+  }
+</style>
+
+<h2>Estrutura de repetição - for</h2>
 <p>
-    Utilizamos para de repetir um conjunto de instruções por um 
+    Utilizada para repetir um conjunto de instruções por um
     número determinado de vezes.
 </p>
-<storng>Exemplo</strong>
+<strong>Exemplo</strong>
 <pre>
     for(inicio, condição, passo){
         //comandos a serem repetidos.
     }
 </pre>
 
-<?php 
-    for($contador = 1; $contador < 10; $contador++){
-        echo "5 x $contador = <br>";
-    }
-?>
+
+
+<div class="grid">
+    <?php
+        for($tabuada = 1; $tabuada <= 10; $tabuada++){
+            echo "<div class='tabuada'>";
+                for($contador = 0; $contador <= 10; $contador++){
+                    $resultado = $tabuada * $contador;
+                    echo "$tabuada x $contador = $resultado<br>";
+                }
+            echo "</div>";
+        }
+    ?>
+</div>
