76ad085af0e60c23ddabc712c3c936b4b3b5e116
Thatix <thais1.carvalho@alunos.ifsuldeminas.edu.br>
2023-05-10 17:08:06 -0300

    atendimento

---
+++ atendimento/dados.php
@@ -0,0 +1,23 @@
+<?php 
+ $aluno1 = [
+    "ra" => 2020123,
+    "nome" => "Poliana",
+    "turma" => "2infoA"
+ ] ;
+
+ $aluno2 = [
+    "ra" => 2020124,
+    "nome" => "Camila",
+    "turma" => "2infoA"
+ ];
+
+ $aluno3 = [
+    "ra" => 2020125,
+    "nome" => "Clara",
+    "turma" => "2infoB"
+ ];
+
+$alunos = [
+    $aluno1, $aluno2, $aluno3
+];
+?>
---
+++ atendimento/tabelaa.php
@@ -0,0 +1,49 @@
+<?php
+
+ require "dados.php" ;
+
+ if(isset($_GET['id'])){
+    //apaga um elemento do vetor que possui o indice 
+    //igual ao valor da variável id
+    $id = $_GET['id'];
+    unset($alunos[$id]);
+ }
+
+ if(isset($_POST['nome'])){
+$alunos[] = [
+   "ra" => $_POST['ra'],
+   "nome" => $_POST['nome'],
+   "turma" => $_POST['turma']
+];
+ }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" width="100%">
+        <tr>
+            <th>RA</th>
+            <th>NOME</th>
+            <th>TURMA</th>
+            <th>AÇÕES</th>
+        </tr>
+
+        <?php foreach($alunos as $key => $aluno){?>
+        <tr align="center">
+            <td><?php echo $aluno["ra"] ?> </td>
+            <td><?php echo $aluno["nome"] ?></td>
+            <td><?php echo $aluno["turma"] ?></td>
+            <td><a href="tabelaa.php?id=<?php echo $key; ?>">Excluir </a></td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
