82a10ec105831822e0c33128bb8a956c1359c43c
Thatix <thais1.carvalho@alunos.ifsuldeminas.edu.br>
2023-05-24 10:37:43 -0300

    template

---
+++ aula13/modelo_3infob_files/sla.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `noticia` (
+	`idnoticia` INT(11) NOT NULL AUTO_INCREMENT,
+	`titulo` VARCHAR(45) NOT NULL COLLATE 'utf8_general_ci',
+	`materia` text NOT NULL COLLATE 'utf8_general_ci',
+	`categoria` VARCHAR(200) NOT NULL COLLATE 'utf8_general_ci',
+	`foto` VARCHAR(255) NULL DEFAULT 'sem_foto.jpg' COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`idnoticia`) USING BTREE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
