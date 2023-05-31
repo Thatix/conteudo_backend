504d44339b6856edd19888e2955d0f2b05098aa8
Thatix <thais1.carvalho@alunos.ifsuldeminas.edu.br>
2023-05-31 10:41:06 -0300

    login

--- aula13/template/menu_restrito.php
+++ aula13/template/menu_restrito.php
@@ -1,3 +1,7 @@
+<?php 
+  require "../noticia/consultar_categorias.php";
+?>
+
 
 <!-- Menu principal -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -9,16 +13,17 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-      <li class="nav-item">
-          <a class="nav-link" href="../site/index.php">Site</a>
-      </li>
+
+      <?php foreach($categorias as $noticia): ?>
+
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?assunto=<?php echo $noticia->categoria; ?>"><?php echo $noticia->categoria; ?></a>
+        </li>   
+
+      <?php endforeach; ?>
 
       <li class="nav-item">
-          <a class="nav-link" href="../noticia/index.php">Notícias</a>
-      </li>
-
-      <li class="nav-item">
-          <a class="nav-link" href="../usuario/index.php">Usuários</a>
+          <a class="nav-link" href="../noticia/index.php">Acesso Restrito</a>
       </li>
 
       </ul>
