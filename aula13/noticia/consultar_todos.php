83ae6eaca4f942ab75ca06269dc58962203a51bb
Thatix <thais1.carvalho@alunos.ifsuldeminas.edu.br>
2023-06-13 08:40:10 -0300

    aula14

--- aula13/noticia/consultar_todos.php
+++ aula13/noticia/consultar_todos.php
@@ -5,10 +5,18 @@
 
 
    //cria uma variável com um comando SQL
-   $SQL = "SELECT * FROM noticia";
+   $SQL = "SELECT * FROM noticia where categoria like ?";
  
    //prepara o comando para ser executado no mysql
    $comando = $conexao->prepare($SQL);
+
+   //pega o valor assunto enviado via GET pela url
+   //ou um valor em branco
+   $assunto = $_GET['assunto'] ?? "";
+   $assunto = "%$assunto%";
+
+   //vincula a variável $assunto com o param ? no SQL
+   $comando->bind_param("s", $assunto);
 
    //executa o comando
    $comando->execute();
