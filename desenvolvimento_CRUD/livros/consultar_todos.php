3cedadc8318dfdd4869865870678d7ad36dbd0c9
Thatix <thais1.carvalho@alunos.ifsuldeminas.edu.br>
2023-06-15 12:05:49 -0300

    surto

---
+++ desenvolvimento_CRUD/livros/consultar_todos.php
@@ -0,0 +1,25 @@
+<?php
+   
+   //importa o arquivo de conexão
+   require_once "../banco/conexao.php";
+
+
+   //cria uma variável com um comando SQL
+   $SQL = "SELECT * FROM usuario";
+ 
+   //prepara o comando para ser executado no mysql
+   $comando = $conexao->prepare($SQL);
+
+   //executa o comando
+   $comando->execute();
+
+   //pegar os resultados da consulta - todas as linhas de resultados
+   $resultados = $comando->get_result();
+
+   //pega a primeira linhas de resultado da consulta
+   $usuarios = [];
+   while  ($usuario = $resultados->fetch_object()){
+    $usuarios[] = $usuario;
+   }
+
+
